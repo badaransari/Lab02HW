605cf1560f901371dbae6254e3d827f57ba19508
soblab <soblab@Whit209-03.it.farmingdale.edu>
2022-02-09 13:25:16 -0500

    Fixed constructors, with to string method for the Student class and able to take input and print output.

---
+++ src/main/java/com/mycompany/csc229/DriverClass.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.csc229;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author soblab
+ */
+public class DriverClass {
+	
+	public static void main(String[] args) {
+		
+		
+		Student std1= new Student("Name",31);
+		
+                Scanner in = new Scanner (System.in);
+                System.out.println("Enter your gpa");
+                
+                
+                System.out.println(std1);
+                        
+                
+                
+                 //This will take gpa as an input from the user
+                 
+		
+		
+	}
+
+   
+
+}
